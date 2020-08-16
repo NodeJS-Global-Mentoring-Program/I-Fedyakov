b3cfa38cbc4eeb0d9169a85a547ffe02e83ee903
Ivan_Fediakov <Ivan_Fediakov@epam.com>
2020-08-16 15:27:52 +0300

    The second task is completed

---
+++ task2.js
@@ -0,0 +1,17 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const sourceFilePath = './csv/books.csv';
+const readable = fs.createReadStream(sourceFilePath, {
+	highWaterMark: 64,
+	encoding: 'utf-8'
+});
+
+const targetFilePath = './books.txt';
+const writable = fs.createWriteStream(targetFilePath, 'utf-8');
+
+const csvConfig = {headers: ['book', 'author', 'price']}
+readable.pipe(csv(csvConfig)).pipe(writable);
+
+readable.on('error', err => console.log(new Error(err).message));
+writable.on('error', err => console.log(new Error(err).message));
